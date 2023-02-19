dbf33251006f0070d26a9e74b97a83c9dc1fa93c
Julian Celeita <andresceleita@yahoo.es>
2023-02-19 00:30:06 -0500

    fetchTodos was typed and added small details

--- hooks/fetchTodos.ts
+++ hooks/fetchTodos.ts
@@ -1,34 +1,47 @@
-import { doc, getDoc } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
-import { useAuth } from '../context/AuthContext'
-import { db } from '../firebase'
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { db } from "../firebase";
 
-export default function useFetchTodos() {
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-    const [todos, setTodos] = useState(null)
- 
-    const { currentUser } = useAuth()
+interface TodoType {
+  id: string;
+  title: string;
+  completed: boolean;
+}
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const docRef = doc(db, 'users', currentUser.uid)
-                const docSnap = await getDoc(docRef)
-                if (docSnap.exists()) {
-                    setTodos(docSnap.data().todos)
-                } else {
-                    setTodos({})
-                }
-            } catch (err) {
-                setError('Failed to load todos')
-                console.log(err)
-            } finally {
-                setLoading(false)
-            }
+interface FetchTodoReturnType {
+  loading: boolean;
+  error: string | null;
+  todos: TodoType[] | null;
+  setTodos: React.Dispatch<React.SetStateAction<TodoType[] | null>>;
+}
+
+export default function useFetchTodos(): FetchTodoReturnType {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [todos, setTodos] = useState<TodoType[] | null>(null);
+
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const docRef = doc(db, "users", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setTodos(docSnap.data().todos);
+        } else {
+          setTodos([]);
         }
-        fetchData()
-    }, [])
+      } catch (err) {
+        setError("Failed to load todos");
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [currentUser.uid]);
 
-    return { loading, error, todos, setTodos }
+  return { loading, error, todos, setTodos };
 }
