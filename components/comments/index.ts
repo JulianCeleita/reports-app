90f78ac1494ba7be19e0bab3b06cdc4cc0fc1ce6
Julian Celeita <andresceleita@yahoo.es>
2023-02-26 18:26:21 -0500

    Comments folder deleted

--- components/comments/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DescriptionComment } from './DescriptionComment';
-export { default as ListComment } from './ListComment';
-export { default as CommentList } from './tools/CommentList';
